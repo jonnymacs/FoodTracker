66bb2107e96d9cb0cc9e67f9468b5ffb0bb4f78f
Jonny Mac <jonnymac@Mac-mini.local>
2017-02-27 22:27:18 -0800

    rating control wip

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jonny Mac on 2/27/17.
+//  Copyright © 2017 Jonny Mac. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
