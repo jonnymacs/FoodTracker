7ba07b0416c0e6e80473dfb6317e8cd9eff4e2d3
Jonathan McAllister <jmcallister@attinteractive.com>
2017-03-06 16:31:05 -0800

    add rating control and rating UI

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,8 +76,5 @@ class ViewController: UIViewController, UITextFieldDelegate,
         present(imagePickerController, animated: true, completion: nil)
     }
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
 }
 
