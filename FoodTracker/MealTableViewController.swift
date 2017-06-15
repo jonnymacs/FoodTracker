de885e7d758f6b540964c02a8a574a08a0408717
Jonathan McAllister <jmcallister@attinteractive.com>
2017-06-14 19:59:25 -0700

    add meal table view controller logic to populate meal table view cells and point the table view controller ui to this class

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -30,24 +30,30 @@ class MealTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+          for: indexPath) as? MealTableViewCell else { fatalError("The dequeued cell is not an instance of MealTableViewCell.") }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+        
         // Configure the cell...
-
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating 
+    
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -96,23 +102,29 @@ class MealTableViewController: UITableViewController {
 
     //MARK: Private Methods
     private func loadSampleMeals() {
-        let photo1 = UIImage(named: "meal1")
-        let photo2 = UIImage(named: "meal2")
-        let photo3 = UIImage(named: "meal3")
-        
-        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
-            fatalError("Unable to instantiate meal1")
+        for i in 0..<3 {
+          let photo = UIImage(named: "meal\(i+1)")
+          guard let meal = Meal(name: "Meal Name \(i+1)", photo: photo, rating: i+2) else { fatalError("Unable to instantiate meal1") }
+          meals.append(meal)
         }
         
-        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
-            fatalError("Unable to instantiate meal2")
-        }
-        
-        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 5) else {
-            fatalError("Unable to instantiate meal3")
-        }
-        
-        meals += [meal1, meal2, meal3]
+        // let photo1 = UIImage(named: "meal1")
+        // let photo2 = UIImage(named: "meal2")
+        // let photo3 = UIImage(named: "meal3")
+        // 
+        // guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+        //     fatalError("Unable to instantiate meal1")
+        // }
+        // 
+        // guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+        //     fatalError("Unable to instantiate meal2")
+        // }
+        // 
+        // guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 5) else {
+        //     fatalError("Unable to instantiate meal3")
+        // }
+        // 
+        // meals += [meal1, meal2, meal3]
         
     }
 }
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,
+class MealViewController: UIViewController, UITextFieldDelegate,
     UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
 
@@ -37,7 +36,6 @@ class ViewController: UIViewController, UITextFieldDelegate,
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
     }
 
     //MARK: UIImagePickerControllerDelegate
